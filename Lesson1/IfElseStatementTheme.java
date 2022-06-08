372bd99b82d12ce981789636195d42a0087df208
quantumwall <liberiamor@gmail.com>
2022-06-08 12:44:02 +0300

    fix ifelse homework

--- Lesson1/IfElseStatementTheme.java
+++ Lesson1/IfElseStatementTheme.java
@@ -23,28 +23,27 @@ public class IfElseStatementTheme {
 
         char nameFirstLetter = name.charAt(0);
         if(nameFirstLetter == 'M') {
-            System.out.printf("Имя начинается на %с\n\n", nameFirstLetter);
+            System.out.printf("Имя начинается на %с\n", nameFirstLetter);
         } else if(nameFirstLetter == 'I') {
-            System.out.printf("Имя начинается на %c\n\n", nameFirstLetter);
+            System.out.printf("Имя начинается на %c\n", nameFirstLetter);
         } else {
-            System.out.println("Имя начинается на букву которую мы не ожидали\n");
+            System.out.println("Имя начинается на букву которую мы не ожидали");
         }
 
-        System.out.println("2. Поиск max и min числа");
+        System.out.println("\n2. Поиск max и min числа");
         int num1 = 34;
         int num2 = 3;
         if(num1 > num2) {
-            System.out.printf("Максимальное число - %d\nМинимальное число - %d\n\n", num1, num2);
+            System.out.printf("Максимальное число - %d\nМинимальное число - %d\n", num1, num2);
         } else if(num1 < num2) {
-            System.out.printf("Максимальное число - %d\nМинимальное число - %d\n\n", num2, num1);
+            System.out.printf("Максимальное число - %d\nМинимальное число - %d\n", num2, num1);
         } else {
-            System.out.printf("%1$.2f равно %1$.2f\n\n", num1);
+            System.out.printf("%1$.2f равно %1$.2f\n", num1);
         }
 
-        System.out.println("3. Работа с числом");
+        System.out.println("\n3. Работа с числом");
         int srcNum = 1612;
         if(srcNum != 0) {
-            System.out.printf("Число %d не является нулем\n", srcNum);
             if(srcNum > 0) {
                 System.out.printf("Число %d положительное\n", srcNum);
             } else {
@@ -52,58 +51,49 @@ public class IfElseStatementTheme {
             }
 
             if(srcNum % 2 == 0) {
-                System.out.printf("Число %d четное\n\n", srcNum);
+                System.out.printf("Число %d четное\n", srcNum);
             } else {
-                System.out.printf("Число %d нечетное\n\n", srcNum);
+                System.out.printf("Число %d нечетное\n", srcNum);
             }
         } else {
-            System.out.printf("Число %d это ноль\n\n", srcNum);
+            System.out.printf("Число %d это ноль\n", srcNum);
         }
 
-        System.out.println("4. Поиск одинаковых цифр в числах");
+        System.out.println("\n4. Поиск одинаковых цифр в числах");
         num1 = 204;
         num2 = 224;
-        boolean isHundredsEquals = num1 / 100 == num2 / 100;
+        boolean isHundredsEquals = (num1 / 100) == (num2 / 100);
         boolean isDozensEquals = (num1 % 100 / 10) == (num2 % 100 / 10);
         boolean isOnesEquals = (num1 % 100 % 10) == (num2 % 100 % 10);
-        if(isHundredsEquals && isDozensEquals && isOnesEquals) {
-            System.out.printf("В числах %d, %d одинаковые цифры %d, %d, %d стоят в разрядах сотен, десятков и единиц соответственно\n\n",
-                num1, num2, num1 / 100, num1 % 100 / 10, num1 % 100 % 10);
-        } else if(isHundredsEquals && isDozensEquals) {
-            System.out.printf("В числах %d, %d одинаковые цифры %d, %d, стоят в разрядах сотен и десятков соответственно\n\n",
-                num1, num2, num1 / 100, num1 % 100 / 10);
-        } else if(isHundredsEquals && isOnesEquals) {
-            System.out.printf("В числах %d, %d одинаковые цифры %d, %d, стоят в разрядах сотен и единиц соответственно\n\n",
-                num1, num2, num1 / 100, num1 % 100 % 10);
-        } else if(isDozensEquals && isOnesEquals) {
-            System.out.printf("В числах %d, %d одинаковые цифры %d, %d, стоят в разрядах десятков и единиц соответственно\n\n",
-                num1, num2, num1 % 100 / 10, num1 % 100 % 10);
-        } else if(isHundredsEquals){
-            System.out.printf("В числах %d, %d одинаковые цифры %d стоят в разрядах сотен\n\n",
-                num1, num2, num1 / 100);
-        } else if(isDozensEquals) {
-            System.out.printf("В числах %d, %d одинаковые цифры %d стоят в разрядах десятков\n\n",
-                num1, num2, num1 % 100 / 10);
-        } else if(isOnesEquals) {
-            System.out.printf("В числах %d, %d одинаковые цифры %d стоят в разрядах единиц\n\n",
-                num1, num2, num1 % 100 % 10);
-        } else {
-            System.out.printf("В числах %d, %d нет одинаковых цифр в одиних и тех же разрядах\n\n", num1, num2);
+        if(isHundredsEquals) {
+            int hundreds = num1 / 100;
+            System.out.printf("В числах %d и %d одинаковые цифры %d в разрядах сотен\n",
+                num1, num2, hundreds);
+        }
+        if(isDozensEquals) {
+            int dozens = num1 % 100 / 10;
+            System.out.printf("В числах %d и %d одинаковые цифры %d в разрядах десятков\n",
+                num1, num2, dozens);
+        }
+        if(isOnesEquals) {
+            int ones = num1 % 100 % 10;
+            System.out.printf("В числах %d и %d одинаковые цифры %d в разрядах единиц\n",
+                num1, num2, ones);
         }
 
-        System.out.println("5. Определение буквы, числа или символа по их коду");
+        System.out.println("\n5. Определение буквы, числа или символа по их коду");
         char character = '\u0057';
         if(character >= 'A' && character <= 'Z') {
-            System.out.printf("%c большая буква\n\n", character);
+            System.out.printf("%c большая буква\n", character);
         } else if(character >= 'a' && character <= 'z') {
-            System.out.printf("%c маленькая буква\n\n", character);
+            System.out.printf("%c маленькая буква\n", character);
         } else if(character >= '0' && character <= '9') {
-            System.out.printf("%c - цифра\n\n", character);
+            System.out.printf("%c - цифра\n", character);
         } else {
-            System.out.printf("%c не буква и не цифра\n\n", character);
+            System.out.printf("%c не буква и не цифра\n", character);
         }
 
-        System.out.println("6. Определение суммы вклада и начисленных банком %");
+        System.out.println("\n6. Определение суммы вклада и начисленных банком %");
         int deposit = 300000;
         int rate;
         if(deposit < 100000) {
@@ -116,9 +106,9 @@ public class IfElseStatementTheme {
         double depositWithInterest = deposit + (deposit * (rate / 100.0));
         System.out.printf("Сумма вклада - %d руб.\n", deposit);
         System.out.printf("Процентная ставка - %d%%\n", rate);
-        System.out.printf("Итоговая сумма - %.2f руб.\n\n", depositWithInterest);
+        System.out.printf("Итоговая сумма - %.2f руб.\n", depositWithInterest);
 
-        System.out.println("7. Определение оценки по предметам");
+        System.out.println("\n7. Определение оценки по предметам");
         int historyRate = 59;
         int programRate= 91;
         int historyScore = 0;
@@ -144,17 +134,17 @@ public class IfElseStatementTheme {
         }
         System.out.printf("%d история \n", historyScore);
         System.out.printf("%d программирование\n", programScore);
-        System.out.printf("Cредний балл оценок по предметам - %d баллов\n", (historyScore + programScore) / 2);
-        System.out.printf("Cредний %% по предметам - %.1f%%\n\n", (historyRate + programRate) / 2.0);
+        System.out.printf("Средний балл оценок по предметам - %d баллов\n", (historyScore + programScore) / 2);
+        System.out.printf("Средний %% по предметам - %.1f%%\n", (historyRate + programRate) / 2.0);
 
-        System.out.println("8. Расчет прибыли");
+        System.out.println("\n8. Расчет прибыли");
         int rentPrice = 5000;
         int productionCosts = 9000;
         int profit = 13000;
-        int netProfit = (profit * 12) - (rentPrice * 12) - 9000;
-        System.out.printf("Прибыл за год: %d руб.\n\n", netProfit);
+        int netProfit = (profit - rentPrice - productionCosts) * 12;
+        System.out.printf("Прибыль за год: %d руб.\n", netProfit);
 
-        System.out.println("9. Определение существования треугольника");
+        System.out.println("\n9. Определение существования треугольника");
         int side1 = 3;
         int side2 = 4;
         int side3 = 5;
@@ -163,7 +153,7 @@ public class IfElseStatementTheme {
         if(side1 + side3 <= side2) isTriangle = false;
         if(side2 + side3 <= side1) isTriangle = false;
         if(!isTriangle) {
-            System.out.printf("Треугольник со сторонами %d, %d, %d не существует\n\n",
+            System.out.printf("Треугольник со сторонами %d, %d, %d не существует\n",
                 side1, side2, side3);
         } else {
             int hypotenuse = side3;
@@ -183,15 +173,15 @@ public class IfElseStatementTheme {
             System.out.println("  \\  ");
             System.out.println("|  \\ ");
             System.out.println("    \\");
-            System.out.println("|____\\\n");
+            System.out.println("|____\\");
         }
 
-        System.out.println("10. Подсчет количества банкнот");
-        int neededSum = 567;
+        System.out.println("\n10. Подсчет количества банкнот");
+        int totalSum = 567;
         int dozensInATM = 5;
-        int hundredsCount = neededSum / 100;
-        int dozensCount = neededSum % 100 / 10;
-        int onesCount = neededSum % 1000 % 10;
+        int hundredsCount = totalSum / 100;
+        int dozensCount = totalSum % 100 / 10;
+        int onesCount = totalSum % 1000 % 10;
         if(dozensInATM < dozensCount) {
             onesCount += (dozensCount - dozensInATM) * 10;
             dozensCount = dozensInATM;
