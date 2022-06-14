9ca1110b41776c8f3ee4ba65d9ba27a5ec142994
quantumwall <liberiamor@gmail.com>
2022-06-14 11:58:04 +0300

    add MyFirstFame

---
+++ Lesson1/MyFirstGame.java
@@ -0,0 +1,19 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int guess = (int) System.currentTimeMillis() % 100 + 1;
+        int max = 99;
+        int min = 0;
+        int answer = max - ((max - min) / 2);
+        while(answer != guess) {
+            if(answer > guess) {
+                System.out.printf("Число %d больше, чем загаданное\n", answer);
+                max = answer;
+            } else {
+                System.out.printf("Число %d меньше, чем загаданное\n", answer);
+                min = answer;
+            }
+            answer = max - ((max - min) / 2);
+        }
+        System.out.println("Вы победили!");
+    }
+}
