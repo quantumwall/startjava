afcdefcce90888d976a7f9a5f29aab52766095a9
quantumwall <liberiamor@gmail.com>
2022-06-16 15:28:25 +0300

    add person.java

---
+++ Lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String sex = "Male";
+    String name = "Richard";
+    double height = 1.80;
+    double wight = 75.0;
+    int age = 33;
+
+    void walk() {
+        System.out.println("I can walk");
+    }
+
+    void sit() {
+        System.out.println("I can sit");
+    }
+
+    void run() {
+        System.out.println("I can run");
+    }
+
+    void speak() {
+        System.out.println("I can speak");
+    }
+
+    void studyJava() {
+        System.out.println("I study Java");
+    }
+}
