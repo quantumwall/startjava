c7953f53062125a5dc5f099ebb0b18460697fe4b
quantumwall <liberiamor@gmail.com>
2022-06-17 13:11:18 +0300

    modify Wolf and WolfTest

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,28 +1,72 @@
 public class Wolf {
     
-    String gender;
-    String alias;
-    int age;
-    int weight;
-    String color;
+    private String gender;
+    private String alias;
+    private int age;
+    private int weight;
+    private String color;
 
-    void howl() {
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+    
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAge(int age) {
+        if(age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void howl() {
         System.out.println("Воет");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Сидит");
     }
 
-    void walk() {
+    public void walk() {
         System.out.println("Идет");
     }
 
-    void run() {
+    public void run() {
         System.out.println("Бежит");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("Охотится");
     }
 }
--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -2,16 +2,16 @@ public class WolfTest {
     
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.gender = "male";
-        wolf.alias = "bloody beast";
-        wolf.age = 3;
-        wolf.weight = 25;
-        wolf.color = "gray";
-        System.out.println("Кличка: " + wolf.alias);
-        System.out.println("Пол: " + wolf.gender);
-        System.out.printf("Возраст: %d лет\n", wolf.age);
-        System.out.printf("Вес: %d кг\n", wolf.weight);
-        System.out.println("Окрас: " + wolf.color);
+        wolf.setGender("male");
+        wolf.setAlias("bloody beast");
+        wolf.setAge(3);
+        wolf.setWeight(25);
+        wolf.setColor("gray");
+        System.out.println("Кличка: " + wolf.getAlias());
+        System.out.println("Пол: " + wolf.getGender());
+        System.out.printf("Возраст: %d лет\n", wolf.getAge());
+        System.out.printf("Вес: %d кг\n", wolf.getWeight());
+        System.out.println("Окрас: " + wolf.getColor());
         wolf.sit();
         wolf.howl();
         wolf.walk();
