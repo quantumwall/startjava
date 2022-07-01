6eb75073f521af93e3fd3c3553fbe931303afde8
quantumwall <liberiamor@gmail.com>
2022-07-01 18:52:55 +0300

    Manage players in array in GuessNumber game

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -5,70 +5,55 @@ import java.util.Scanner;
 public class GuessNumber {
     
     private int targetNumber;
-    private Player player1;
-    private Player player2;
+    private Player[] players;
      
-    public GuessNumber(Player p1, Player p2) {
-        player1 = p1;
-        player2 = p2;
+    public GuessNumber(Player... players) {
+        this.players = new Player[players.length]; 
+        for(int i = 0; i < players.length; i++) {
+            this.players[i] = players[i];
+        }
     }
 
     public void start() {
         prepareNewGame();
+        int attempt = 0;
+        int maxAttempts = 9;
         System.out.println("У каждого игрока есть 10 попыток, чтобы угадать число");
         Scanner input = new Scanner(System.in);
         targetNumber = (int) (Math.random() * 100 + 1);
         int playerNumber;
-        int player1Attempt = 0;
-        int player2Attempt = 0;
         while(true) {
-            if(player1Attempt == 10 && player2Attempt == 10) {
+            if(attempt > maxAttempts) {
                 System.out.println("Попытки закончились. Game over.");
-                break;
+                return;
             }
-            
-            if(player1Attempt < 10) {
-                System.out.printf("Ход игрока %s. Введите число: ", player1.getName());
-                player1.setNumber(input.nextInt(), player1Attempt);
-                playerNumber = player1.getNumber(player1Attempt);
+            for(Player player : players) {
+                System.out.printf("Ход игрока %s. Введите число: ", player.getName());
+                player.setNumber(input.nextInt(), attempt);
+                playerNumber = player.getNumber(attempt);
                 if(playerNumber == targetNumber) {
-                    System.out.printf("Игрок %s угадал число с %d попытки\n", player1.getName(), player1Attempt + 1);
-                    break;
+                    System.out.printf("Игрок %s угадал число с %d попытки\n", player.getName(), attempt + 1);
+                    player.setAttempt(attempt + 1);
+                    return;
                 } else if(playerNumber < targetNumber) {
-                    System.out.printf("%s: Число %d меньше загаданного\n", player1.getName(), playerNumber);
-                    player1.setAttempt(++player1Attempt);
+                    System.out.printf("%s: Число %d меньше загаданного\n", player.getName(), playerNumber);
+                    player.setAttempt(attempt + 1);
                 } else {
-                    System.out.printf("%s: Число %d больше загаданного\n", player1.getName(), playerNumber);
-                    player1.setAttempt(++player1Attempt);
+                    System.out.printf("%s: Число %d больше загаданного\n", player.getName(), playerNumber);
+                    player.setAttempt(attempt + 1);
                 }
-            } else {
-                System.out.printf("У игрока %s закончились попытки", player1.getName());
-            }
-            
-            if(player2Attempt < 10) {
-                System.out.printf("Ход игрока %s. Введите число: ", player2.getName());
-                player2.setNumber(input.nextInt(), player2Attempt);
-                playerNumber = player2.getNumber(player2Attempt);
-                if(playerNumber == targetNumber) {
-                    System.out.printf("Игрок %s угадал число с %d попытки\n", player2.getName(), player2Attempt + 1);
-                    break;
-                } else if(playerNumber < targetNumber) {
-                    System.out.printf("%s: Число %d меньше загаданного\n", player2.getName(), playerNumber);
-                    player2.setAttempt(++player2Attempt);
-                } else {
-                    System.out.printf("%s: Число %d больше загаданного\n", player2.getName(), playerNumber);
-                    player2.setAttempt(++player2Attempt);
+                if(attempt + 1 > maxAttempts) {
+                    System.out.printf("У игрока %s закончились попытки\n", player.getName());
                 }
-            } else {
-                System.out.printf("У игрока %s закончились попытки", player2.getName());
             }
+            attempt++;
         }
     }
     
     public void prepareNewGame() {
-        player1.resetNumbers();
-        player2.resetNumbers();
-        player1.setAttempt(0);
-        player2.setAttempt(0);
+        for(Player player : players) {
+            player.resetNumbers();
+            player.setAttempt(0);
+        }
     }
 }
--- src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumberTest.java
@@ -5,23 +5,24 @@ import java.util.Scanner;
 public class GuessNumberTest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Введите имя первого игрока: ");
-        Player player1 = new Player(input.nextLine());
-        System.out.print("Введите имя второго игрока: ");
-        Player player2 = new Player(input.nextLine());
-        GuessNumber game = new GuessNumber(player1, player2);
+        System.out.print("Введите количество игроков: ");
+        int playersCount = input.nextInt();
+        Player[] players = new Player[playersCount];
+        input.nextLine();
+        for(int i = 0; i < playersCount; i++) {
+            System.out.printf("Введите имя игрока %d: ", i + 1);
+            players[i] = new Player(input.nextLine());
+        }
+        GuessNumber game = new GuessNumber(players);
         String repeat = "no";
         do {
             game.start();
-            int[] player1Numbers = player1.getNumbers();
-            int[] player2Numbers = player2.getNumbers();
-            System.out.printf("Ответы игрока %s\n", player1.getName());
-            for(int num : player1Numbers) {
-                System.out.printf("%d ", num);
-            }
-            System.out.printf("\nОтветы игрока %s\n", player2.getName());
-            for(int num : player2Numbers) {
-                System.out.printf("%d ", num);
+            for(Player player : players) {
+                System.out.printf("Ответы игрока %s\n", player.getName());
+                for(int num : player.getNumbers()) {
+                    System.out.printf("%d ", num);
+                }
+                System.out.println();
             }
             do {
                 System.out.print("\nХотите повторить игру? [yes/no]: ");
